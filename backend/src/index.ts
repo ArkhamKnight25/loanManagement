742bf675baa93a4be9f4f88b43502dac3b53bc56
unknown <ArkhamKnight2512>
2025-04-07 13:32:13 +0530

    delete unnecessary files that was used for trial

--- backend/src/index.ts
+++
@@ -1,8 +0,0 @@
-import authRoutes from './routes/auth.routes';
-import loansRoutes from './routes/loans.routes';
-import adminRoutes from './routes/admin.routes';
-
-// Setup routes
-app.use('/api/auth', authRoutes);
-app.use('/api/loans', loansRoutes);
-app.use('/api/admin', adminRoutes); 
